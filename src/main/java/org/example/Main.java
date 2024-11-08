7a0bfe4ef2c2876f5773b8c0edd0301fa7aa92d8
szinaierik <szinai.erik@silentsignal.hu>
2024-11-08 12:55:42 +0100

    Implemented the main function which accepts two arguments for CLI testing.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,8 @@ import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.logging.Logging;
 import burp.api.montoya.utilities.Utilities;
 
+import java.util.List;
+
 public class Main implements BurpExtension {
 
     MontoyaApi api;
@@ -20,6 +22,18 @@ public class Main implements BurpExtension {
         api.extension().setName("Custom Insertion Point Provider");
         logging.logToOutput("Custom Insertion Point Provider");
         api.scanner().registerInsertionPointProvider(new CustomAuditInsertionPointProvider(api));
+    }
+
+    public static void main(String [] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar file.jar 'dummy parameter value' 'dummy payload'");
+        } else{
+            Test test = new Test(args[0]);
+            List<EncodingTree> modified = test.dummyBuildWithPayload(args[1]);
+            for (EncodingTree node : modified) {
+                System.out.println("Modified parameter is the following: " + node.getNode().getValue());
+            }
+        }
     }
 }
 
