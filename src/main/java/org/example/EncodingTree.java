e0b6b075eee990a2fec82ee0e3ee6ef236b3d057
szinaierik <szinai.erik@silentsignal.hu>
2024-11-08 12:54:14 +0100

    Modified the code due to the new EncodingHelper class.

--- src/main/java/org/example/EncodingTree.java
+++ src/main/java/org/example/EncodingTree.java
@@ -5,32 +5,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Tree-like class to store encodings and their parent-child hierarchy
-public class EncodingTree implements Serializable {
-    private final HandleEncoding node;
+public class EncodingTree{
+    private HandleEncoding node;
     private final List<EncodingTree> children;
     private EncodingTree parent;
     private boolean isLeaf = false;
-    private final EncodingFactory encodingFactory;
+    private final EncodingHelper encodingHelper;
+
+    public EncodingTree(){
+        this.encodingHelper = new EncodingHelper();
+        this.children = new ArrayList<>();
+    }
 
     // Constructor
     public EncodingTree(HandleEncoding node) {
-        encodingFactory = new EncodingFactory();
-        this.node = encodingFactory.createNewEncoding(node);
+        encodingHelper = new EncodingHelper();
+        this.node = encodingHelper.getEncodingFactory().createNewEncoding(node);
         children = new ArrayList<>();
     }
 
     // Constructor with an additional parent parameter
     public EncodingTree(HandleEncoding node, EncodingTree parent) {
-        encodingFactory = new EncodingFactory();
-        this.node = encodingFactory.createNewEncoding(node);
+        encodingHelper = new EncodingHelper();
+        this.node = encodingHelper.getEncodingFactory().createNewEncoding(node);
         this.parent = parent;
         this.children = new ArrayList<>();
     }
 
     public EncodingTree(EncodingTree tree){
-        this.encodingFactory = new EncodingFactory();
+        encodingHelper = new EncodingHelper();
         this.children = new ArrayList<>();
-        this.node = encodingFactory.createNewEncoding(tree.getNode());
+        this.node = encodingHelper.getEncodingFactory().createNewEncoding(tree.getNode());
         if (tree.getParent() != null) {
             this.parent = tree.getParent();
         }
@@ -42,7 +47,7 @@ public class EncodingTree implements Serializable {
 
     // Adding a child to the node
     public void addChild(HandleEncoding child) {
-        EncodingTree childNode = new EncodingTree(encodingFactory.createNewEncoding(child), this);
+        EncodingTree childNode = new EncodingTree(encodingHelper.getEncodingFactory().createNewEncoding(child), this);
         children.add(childNode);
     }
 
@@ -117,5 +122,51 @@ public class EncodingTree implements Serializable {
             node = node.parent;
         }
         return node;
+    }
+
+    // Recursive method to populate the tree
+    public EncodingTree fillTreeRecursively(EncodingTree currentNode, String value){
+        // Check if the current node is null
+        // It means we are at the root
+        if (currentNode.getNode() == null){
+            // Find the encoding for the current value
+            HandleEncoding encoding = encodingHelper.findEncoding(value, "");
+            // Creating the node based on the encoding
+            currentNode = new EncodingTree(encoding);
+            // Finding its children
+            currentNode = findChildren(currentNode);
+            List<EncodingTree> children = currentNode.getChildren();
+            for (EncodingTree node : children){
+                // Recursively traverse each child
+                fillTreeRecursively(node, node.getNode().getValue());
+            }
+        } else{
+            currentNode = findChildren(currentNode);
+            List<EncodingTree> children = currentNode.getChildren();
+            for (EncodingTree node : children){
+                fillTreeRecursively(node, node.getNode().getValue());
+            }
+        }
+        return currentNode;
+    }
+
+    // Method to find the children of a node
+    private EncodingTree findChildren(EncodingTree currentNode){
+        HandleEncoding currentEncoding = currentNode.getNode();
+        // Extracting the values
+        List<KeyValueTuple> kvt = currentEncoding.getValues();
+        // If the list is empty it means it doesn't have any children
+        if (kvt.isEmpty()) {
+            // So it's a leaf node
+            currentNode.setLeaf(true);
+        }else{
+            // Otherwise traverse through its children
+            // Find the encoding for every child and add them to the node as children
+            for (KeyValueTuple keyValueTuple : kvt){
+                currentNode.addChild(encodingHelper.findEncoding(keyValueTuple.getValue(), keyValueTuple.getKey()));
+            }
+        }
+        // Returning the current node with its children
+        return currentNode;
     }
 }
