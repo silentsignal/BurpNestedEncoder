6c1d0170f7075e48b5f3167bb5283391c64c6719
szinaierik <szinai.erik@silentsignal.hu>
2024-11-08 12:53:20 +0100

    Created a new class for testing purposes through command line.

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    private final String paramValue;
+    private EncodingTree root;
+
+    public Test(String paramValue){
+        this.paramValue = paramValue;
+        this.root = new EncodingTree();
+    }
+
+    public List<EncodingTree> dummyBuildWithPayload(String payload){
+        List<EncodingTree> encodingTrees = new ArrayList<>();
+        root = root.fillTreeRecursively(root, paramValue);
+        List<EncodingTree> leafNodes = root.getLeafNodes();
+        for (EncodingTree leafNode : leafNodes) {
+            EncodingTree temp = leafNode.updateNode(leafNode.getNode().getValue(), payload);
+            temp = temp.findRoot(temp);
+            encodingTrees.add(temp);
+        }
+        return encodingTrees;
+    }
+}
